1dfa11a806a8487c076baee8cd79fe1a55aa75c5
vamsi9618245033 <147905983+vamsi9618245033@users.noreply.github.com>
2025-04-04 10:48:58 +0530

    Update settings.py

--- aprems-master/aprems/aprems/settings.py
+++ aprems-master/aprems/aprems/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-x8v@o6g%0-^0cj&8jp06*6h1rrd8gjynw-zsaa2ae8j$8u&0=%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
